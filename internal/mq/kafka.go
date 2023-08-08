4a6cc41156c7cca57c9146cd5cac02d458ba4eda
pot-code <136290354@qq.com>
2023-08-08 15:36:25 +0800

    feat: watermill logging interface

--- internal/mq/kafka.go
+++ internal/mq/kafka.go
@@ -6,9 +6,10 @@ import (
 	"github.com/IBM/sarama"
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
+	"github.com/rs/zerolog"
 )
 
-func NewKafkaSubscriber(brokers []string, consumerGroup string) *kafka.Subscriber {
+func NewKafkaSubscriber(brokers []string, consumerGroup string, l zerolog.Logger) *kafka.Subscriber {
 	config := kafka.DefaultSaramaSubscriberConfig()
 	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 
@@ -17,20 +18,48 @@ func NewKafkaSubscriber(brokers []string, consumerGroup string) *kafka.Subscribe
 		Unmarshaler:           kafka.DefaultMarshaler{},
 		OverwriteSaramaConfig: config,
 		ConsumerGroup:         consumerGroup,
-	}, watermill.NewStdLogger(true, true))
+	}, NewWatermillLogger(l))
 	if err != nil {
 		panic(fmt.Errorf("error creating kafka subscriber: %w", err))
 	}
 	return sub
 }
 
-func NewKafkaPublisher(brokers []string) *kafka.Publisher {
+func NewKafkaPublisher(brokers []string, l zerolog.Logger) *kafka.Publisher {
 	pub, err := kafka.NewPublisher(kafka.PublisherConfig{
 		Brokers:   brokers,
 		Marshaler: kafka.DefaultMarshaler{},
-	}, watermill.NewStdLogger(true, true))
+	}, NewWatermillLogger(l))
 	if err != nil {
 		panic(fmt.Errorf("error creating kafka publisher: %w", err))
 	}
 	return pub
+}
+
+type watermillLogger struct {
+	l zerolog.Logger
+}
+
+func NewWatermillLogger(l zerolog.Logger) watermill.LoggerAdapter {
+	return &watermillLogger{l: l}
+}
+
+func (w *watermillLogger) Error(msg string, err error, fields watermill.LogFields) {
+	w.l.Err(err).Fields(map[string]interface{}(fields)).Msg(msg)
+}
+
+func (w *watermillLogger) Info(msg string, fields watermill.LogFields) {
+	w.l.Info().Fields(map[string]interface{}(fields)).Msg(msg)
+}
+
+func (w *watermillLogger) Debug(msg string, fields watermill.LogFields) {
+	w.l.Debug().Fields(map[string]interface{}(fields)).Msg(msg)
+}
+
+func (w *watermillLogger) Trace(msg string, fields watermill.LogFields) {
+	w.l.Trace().Fields(map[string]interface{}(fields)).Msg(msg)
+}
+
+func (w *watermillLogger) With(fields watermill.LogFields) watermill.LoggerAdapter {
+	return &watermillLogger{l: w.l.With().Fields(map[string]interface{}(fields)).Logger()}
 }
