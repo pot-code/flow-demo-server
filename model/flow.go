9e1dcbe1a475ae5231b3263b9ae452a458ad1365
pot-code <136290354@qq.com>
2023-08-18 17:55:53 +0800

    feat(model): add timestamp to flow and user

--- model/flow.go
+++ model/flow.go
@@ -2,18 +2,22 @@ package model
 
 import (
 	"gobit-demo/internal/uuid"
+	"time"
 
 	"gorm.io/gorm"
 )
 
 type Flow struct {
-	ID          UUID   `gorm:"primaryKey;type:BIGINT UNSIGNED" json:"id,omitempty"`
-	Name        string `gorm:"index,not null,size:32" json:"name,omitempty"`
-	Description string `json:"description,omitempty"`
-	Nodes       string `json:"nodes,omitempty"`
-	Edges       string `json:"edges,omitempty"`
-	OwnerID     *UUID  `json:"owner_id,omitempty"`
-	Owner       *User  `gorm:"foreignKey:OwnerID" json:"owner,omitempty"`
+	ID          UUID           `gorm:"primaryKey;type:BIGINT UNSIGNED" json:"id,omitempty"`
+	Name        string         `gorm:"index,not null,size:32" json:"name,omitempty"`
+	Description string         `json:"description,omitempty"`
+	Nodes       string         `json:"nodes,omitempty"`
+	Edges       string         `json:"edges,omitempty"`
+	OwnerID     *UUID          `json:"owner_id,omitempty"`
+	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at,omitempty"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at,omitempty"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
+	Owner       *User          `gorm:"foreignKey:OwnerID" json:"owner,omitempty"`
 }
 
 func (f *Flow) BeforeCreate(tx *gorm.DB) error {
--- model/user.go
+++ model/user.go
@@ -2,18 +2,22 @@ package model
 
 import (
 	"gobit-demo/internal/uuid"
+	"time"
 
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID       UUID    `gorm:"primaryKey;type:BIGINT UNSIGNED" json:"id,omitempty"`
-	Name     string  `json:"name,omitempty"`
-	Username string  `gorm:"uniqueIndex,not null,size:12" json:"username,omitempty"`
-	Password string  `gorm:"not null" json:"password,omitempty"`
-	Mobile   string  `gorm:"uniqueIndex,not null,size:11" json:"mobile,omitempty"`
-	Disabled bool    `json:"disabled,omitempty"`
-	Roles    []*Role `gorm:"many2many:user_roles" json:"roles,omitempty"`
+	ID        UUID           `gorm:"primaryKey;type:BIGINT UNSIGNED" json:"id,omitempty"`
+	Name      string         `json:"name,omitempty"`
+	Username  string         `gorm:"uniqueIndex,not null,size:12" json:"username,omitempty"`
+	Password  string         `gorm:"not null" json:"password,omitempty"`
+	Mobile    string         `gorm:"uniqueIndex,not null,size:11" json:"mobile,omitempty"`
+	Disabled  bool           `json:"disabled,omitempty"`
+	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at,omitempty"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at,omitempty"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	Roles     []*Role        `gorm:"many2many:user_roles" json:"roles,omitempty"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) error {
