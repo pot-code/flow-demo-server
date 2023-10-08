53d949894895c64d89d4ae945c0965d5e7b6c9dd
pot-code <136290354@qq.com>
2023-10-08 16:04:14 +0800

    refactor: UnAuthorizedError Error

--- services/auth/error.go
+++ services/auth/error.go
@@ -1,12 +1,8 @@
 package auth
 
-import (
-	"fmt"
-)
-
 type UnAuthorizedError struct {
 }
 
 func (e UnAuthorizedError) Error() string {
-	return fmt.Sprintf("unauthorized")
+	return "unauthorized"
 }
